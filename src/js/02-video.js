1cbedd4a1f5eb09f9fcb1357356afb9319a94e9f
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-02-19 15:33:08 +0100

    +throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('timeupdate', function (data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-});
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
 
 player
   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
